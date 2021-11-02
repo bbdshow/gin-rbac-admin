a81f9df367f3001ee70b1b0723ac839995164158
bbdshow <bbdshow@outlook.com>
2021-11-02 14:03:54 +0800

    fix

--- src/assets/utils/fTime.js
+++ src/assets/utils/fTime.js
@@ -4,6 +4,6 @@ const fTime = (stamp) => {
   if (!stamp) {
     return ''
   }
-  return m(stamp * 1000).format('YYYY-MM-DD  hh:mm:ss')
+  return m(stamp * 1000).format('YYYY-MM-DD  HH:mm:ss')
 }
 export default fTime
